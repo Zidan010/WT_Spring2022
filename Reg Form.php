9e24d92a95d2c7c956a4c24f5b78f4f12af494e4
Zidan010 <sazidan559@gmail.com>
2022-01-30 11:27:47 +0600

    uploading Reg Form

---
+++ Reg Form.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+
+<h1>Registration Form</h1><hr>
+<table>
+<form>
+ First Name: <input type="text" name="fname"><br><br>
+ Last Name:<input type="text" name="lname"><br><br>
+ Age: <input type="number" name="age"><br>
+ <br>
+ Designation : <input type="radio" name="Designation">Junior Programmer
+ <input type="radio" name="Designation">Senior Programmer
+ <input type="radio" name="Designation">Project Leader <br><br>
+ Email:<input type="email" name="email"><br><br>
+ Password:<input type="password" name="password"><br><br>
+ Please choose a file <input type="file" name="Choose File"><br><br>
+ <input type="submit">
+            <input type="Reset"><br>
+
+</form>
+
+
+</table>
+</body>
+</html>
